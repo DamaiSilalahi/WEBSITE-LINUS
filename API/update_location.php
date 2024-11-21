6aea3114e7e54635b823ac2f6c2a5fa4dbe924fc
KatherinaNasraniBarus <katherinanasrani@gmail.com>
2024-11-22 02:15:39 +0700

    memperbaiki getstop dan update location serta menambakan get route

--- API/update_location.php
+++ API/update_location.php
@@ -7,12 +7,31 @@ if (!isset($_SESSION['loggedin'])){
     exit;
 }
 
-$plate_number = $_SESSION['plate_number'];
+$plat_nomor = $_SESSION['plat_nomor'];
+$username = $_SESSION ['username'];
 $lat = $_POST['lat'];
 $lng = $_POST['lng'];
 
-$stmt = $conn->prepare("UPDATE drivers SET latitude = ?, longitude = ? WHERE plate_number = ?");
-$stmt->bind_param("dds", $lat, $lng, $plate_number);
-$stmt->execute();
-$stmt->close();
+try {
+    $query = " UPDATE driver_location SET latitude = :lat, longitude = :lng WHERE bus_id IN (SELECT id FROM bus WHERE plat_nomor = :plat_nomor)";
+
+$stmt = $conn->prepare($query);
+$stmt->bindParam(":lat", $lat, PDO::PARAM_STR); 
+$stmt->bindParam(":lng", $lng, PDO::PARAM_STR); 
+$stmt->bindParam(":plat_nomor", $plat_nomor, PDO::PARAM_STR); 
+
+if ($stmt->execute()){
+    $response = array(
+        'plat_nomor' => $plat_nomor,
+        'lat' => $lat,
+        'lng' => $lng,
+        'username' => $username
+    );
+    echo json_encode($response);
+} else {
+    echo "Error Updating Location.";
+}
+} catch (PDOException $e) {
+    echo "Error : " . $e->getMessage();
+}
 ?>
