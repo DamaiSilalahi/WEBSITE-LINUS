17956c3db90e1580810968330817cfc2ecb66b37
DamaiSilalahi <damailalahi0406@gmail.com>
2024-11-25 14:22:00 +0700

    menambahkan path ke css

--- admin/signUp.php
+++ admin/signUp.php
@@ -40,6 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['sign_up'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up Admin</title>
+    <link rel="stylesheet" href="auth.css">
 </head>
 <body>
     <div class="imagehalf"></div>
