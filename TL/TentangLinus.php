9455521fc9ac26f67405a8f67a9afbc54f927b20
laurenpatriciaa <laurenpatricia166@gmail.com>
2024-12-01 23:33:19 +0700

    update tulisan di sidebar

--- TL/TentangLinus.php
+++ TL/TentangLinus.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/TL.css">
     <link rel="stylesheet" href="../css/dm_TL.css">
+    <link rel="stylesheet" href="../css/dm_main.css">
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/stylenav.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
@@ -63,7 +64,7 @@
       
       <aside id="sidebar" class="sidebar">
           <a href="javascript:void(0)" class="closebtn" onclick="toggleSidebar()">&times;</a>
-          <a href="TentangLinus.php" onclick="openTentangLinus()">Tentang Linus</a>
+          <a href="TentangLinus.php" onclick="openTentangLinus()">Tentang Linus</a> 
         </aside>
     <a href="../faq/faq.php">
        <div class="faq-button">
